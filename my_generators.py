167c978df7e9f27a1c749472593c6d66f629aed7
Anthony Garcia <anthonygarcia918@hotmail.com>
2021-08-09 16:24:18 -0600

    Added template.py

--- my_generators.py
+++ my_generators.py
@@ -5,6 +5,55 @@ Date   : 8/9/2021
 Purpose:
 """
 
+from math import sqrt
+
+
+def take(count, iterable):
+    """
+    Take items for the front of the iterable
+    :param count: The maximum number or items retrieved
+    :param iterable: The source series
+    :yield: At most 'count' items for 'iterable'
+    """
+    counter = 0
+    for item in iterable:
+        if counter == count:
+            return
+        counter += 1
+        yield item
+
+
+def run_take():
+    items = [2, 4, 6, 8, 10]
+    for item in take(3, items):
+        print(item)
+
+
+def distinct(iterable):
+    """
+    Return unique items by eliminating duplicates
+    :param iterable: The source series
+    :yield: Unique elements in order from 'iterable'
+    """
+    seen = set()
+    for item in iterable:
+        if item in seen:
+            continue
+        yield item
+        seen.add(item)
+
+
+def run_distinct():
+    items = [5, 7, 7, 6, 5, 5]
+    for item in distinct(items):
+        print(item)
+
+
+def run_pipeline():
+    items = [3, 6, 6, 2, 1, 1]
+    for item in take(3, distinct(items)):
+        print(item)
+
 
 def gen246():
     print('About to yield 2')
@@ -29,15 +78,16 @@ def first(iterable):
     except StopIteration:
         raise ValueError('Iterable is empty')
 
+
 # --------------------------------------------------
 def main():
     """Review Iterator and Generators"""
-    #iterable = ['spring', 'summer', 'fall', 'winter']
-    #iterator = iter(iterable)
-
-    g = gen123()
-    for v in gen123():
-        print(f'Value is {v}')
+    # Task: Create a list of the first 1 million square numbers
+    m_sq = (x*x for x in range(1, 1001))
+    l_sq = list(m_sq)
+    print(type(m_sq))
+    print(type(l_sq))
+    print(f'The sum of the first 1000 square numbers is: {sum(l_sq)}')
 
 # --------------------------------------------------
 if __name__ == '__main__':
