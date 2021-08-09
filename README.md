508cd9e3157106980c70eb7eb567eda565f1c1f0
Anthony Garcia <anthonygarcia918@hotmail.com>
2021-08-09 09:42:15 -0600

    Updated README.md with instructions and updated hello.py as a module

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # python_hafb
-Python-HAFB
+
+
+## Instructor project
+https://github.com/hugo-wsu/python-hafb
+
+## Local installations
+Need to open git bash and run the following command
+```bash
+pip install pytest
+```
+Reload 'Pycharm'
+```bash
+$Env:Path += ";C:\users\<username>\appdata\Roaming\Python\Python39\Scripts\"
+```
--- hello.py
+++ hello.py
@@ -1,2 +1,30 @@
-# Purpose: Print hello world
-print("Hello Waldo")
+#!/usr/bin/env python3
+"""
+Author: Anthony Garcia
+Purpose: Print hello ARG
+"""
+
+import argparse
+
+
+def get_args():
+    """
+    Get the command-line arguments
+    :return: list of parsed arguments
+    """
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default="World", help='Name to greet')
+    return parser.parse_args()
+
+
+def main():
+    """
+    Main execution
+    """
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == '__main__':
+    main()
